61afb4b41f6c7f5c32543099a31b5cd2b819d0aa
Gregz9 <gregorkajda@gmail.com>
2023-05-05 09:52:33 +0200

    Power iteration method for findind all eigenvalues and eigenvectors of a matrix
    Signed-off-by: Gregz9 <gregorkajda@gmail.com>

--- src/solvers.py
+++ src/solvers.py
@@ -23,8 +23,48 @@ def analytical_solver(mat):
     return Eigvalues, EigVectors
 
 
+def normalize(x):
+    fac = abs(x).max()
+    x_n = x / x.max()
+    return fac, x_n
+
+
+def power_iter(mat, iters=100):
+    x = [1, 1]
+    y = [1, 1]
+    for i in range(iters):
+        x = np.dot(mat, x)
+        lambda_1, x = normalize(x)
+
+    print("Eigenvalue:", lambda_1)
+    print("Eigenvector:", x)
+
+
+def power_iteration(a, num_iterations):
+    n = len(a)
+    eigenvalues = []
+    eigenvectors = []
+
+    for i in range(n):
+        x = np.random.rand(n)
+
+        for j in range(num_iterations):
+            x = np.dot(a, x)
+            eigenvalue, x = normalize(x)
+
+        eigenvectors.append(x)
+        eigenvalues.append(eigenvalue)
+        a = a - eigenvalue * np.outer(x, x)
+
+    print("Eigenvalues:", eigenvalues)
+    print("Eigenvectors:", eigenvectors)
+    return eigenvalues, eigenvectors
+
+
 if __name__ == "__main__":
     hamil = hamilitonian(dim=2, e0=0.0, e1=4.0, Xdiag=3, Xnondiag=0.2)
     eig_vals, eig_vecs = analytical_solver(hamil)
     print(eig_vals)
     print(eig_vecs)
+    power_iter(hamil)
+    power_iteration(hamil, 100)
