32300f70d5c1a5f7063448af874763458be566c5
Gregz9 <gregorkajda@gmail.com>
2023-05-14 15:10:29 +0200

    Almost finished with demonstration of bell pairs
    Signed-off-by: Gregz9 <gregorkajda@gmail.com>

--- tests/bell_states.py
+++ tests/bell_states.py
@@ -64,7 +64,7 @@ if __name__ == "__main__":
         "Here we force one of the quibts in the bell pair to collapse to state |0> or state |1> \n"
     )
 
-    print("Measuring first qubit: ")
+    print("Measuring first qubit of the entangled pair: ")
     print("-------------------------------------------------------")
     prob, state = projective_measurement(b00, 0, 0)
     print(f"Probability of the wave function collapsing to |0> : {prob}")
@@ -74,5 +74,54 @@ if __name__ == "__main__":
     print(f"Probability of the wave function collapsing to |1> : {prob}")
     print(f"State of the bell pair after measurement: {state}\n")
 
-    # print(counts)
-    # print(meas_probs)
+    print("Measuring the second qubit of the pair: ")
+    print("-------------------------------------------------------")
+    prob, state = projective_measurement(b00, 1, 0)
+    print(f"Probability of the wave function collapsing to |0> : {prob}")
+    print(f"State of the bell pair after measurement: {state}\n")
+
+    prob, state = projective_measurement(b00, 1, 1)
+    print(f"Probability of the wave function collapsing to |1> : {prob}")
+    print(f"State of the bell pair after measurement: {state}\n")
+
+    print(
+        "As we can see, no matter which of the qubits of the bell pair we measure,\n"
+        + "the bell pair will always collapse to one the two entangled states, as indicated\n"
+        + "by the measurements performed above.\n"
+    )
+    print(
+        "---------------------------------------------------------------------------------"
+    )
+
+    print(
+        "What happens then if we apply the CNOT gate and the Hadamard gate to the first bell pair b00?\n\n"
+    )
+
+    print("Constructing and applying the Controlled-NOT gate to the first bell pair: ")
+    print(
+        "---------------------------------------------------------------------------------"
+    )
+
+    CNOT = Cnot()
+    print(CNOT)
+
+    print("Result of applying the CNOT gate to b00: ")
+    CNOT_b00 = CNOT @ b00
+    print(CNOT_b00)
+
+    print(
+        "Result of applying the Hadamard gate to b00, after having applied the CNOT: "
+    )
+    HCNOT_b00 = np.kron(Hadamard(), Identity()) @ CNOT_b00
+    HCNOT_b00 = np.where(HCNOT_b00 > 1e-10, HCNOT_b00, 0)
+    print(HCNOT_b00)
+
+    print("Result of applying the Hadamard gate to b00: ")
+    H_b00 = np.kron(Hadamard(), np.eye(2)) @ b00
+    print(H_b00)
+
+    print(
+        "Result of applying the CNOT gate to b00, after having applied the Hadamard gate: "
+    )
+    CNOTH_b00 = CNOT @ H_b00
+    print(CNOTH_b00)
