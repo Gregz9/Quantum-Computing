aa9e35e7e28788db0cb2c86913961a0c08e7e3d8
Gregz9 <gregorkajda@gmail.com>
2023-05-16 18:17:50 +0200

    Finished analytical and numerical solution of the hamiltonian
    Signed-off-by: Gregz9 <gregorkajda@gmail.com>

--- src/runs/2qubits_ana_num.py
+++ src/runs/2qubits_ana_num.py
@@ -7,25 +7,36 @@ from scipy.optimize import minimize
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
-basis0 = np.array([1, 0])
-basis1 = np.array([0, 1])
-
-op0 = np.kron(basis0, Identity())
-op1 = np.kron(basis1, Identity())
-
 H = hamiltonian_2qubit(1.0, 2.0, 3.0, np.array([0.0, 2.5, 6.5, 7.0]))
-# print(H)
 lmbds = np.linspace(0.0, 1.0, 50)
 eigvals_an = np.zeros((len(lmbds), H.shape[1]))
+eigvals_num1 = np.zeros((len(lmbds), H.shape[1]))
+eigvals_num2 = np.zeros((len(lmbds), H.shape[1]))
 entropy = np.zeros((len(lmbds), H.shape[1]))
 
 for i, lmbd in enumerate(lmbds):
     H = hamiltonian_2qubit(lmbd, 2.0, 3.0, np.array([0.0, 2.5, 6.5, 7.0]))
     eigvals, eigvecs = np.linalg.eig(H)
-    perm = eigvals.argsort()
-    eigvals_an[i] = eigvals[perm]
-    eigvecs = eigvecs[:, perm]
+    eig_num1, _ = power_iteration(H, 400)
+    eig_num2 = QR_solver(H)
+    perm_an = eigvals.argsort()
+    perm_num1 = eig_num1.argsort()
+    perm_num2 = eig_num2.argsort()
+    eigvals_an[i] = eigvals[perm_an]
+    eig_num1 = eig_num1[perm_num1]
+    eig_num2 = eig_num2[perm_num2]
+    eigvals_num1[i] = eig_num1
+    eigvals_num2[i] = eig_num2
+    eigvecs = eigvecs[:, perm_an]
     entropy[i] = von_neumann(eigvals, eigvecs, 0)
+
+fig, axs = plt.subplots(1, 1, figsize=(8, 8))
+for i in range(4):
+    axs.plot(lmbds, eigvals_an[:, i], label=f"$E_{i}$")
+axs.set_xlabel(r"$\lambda$")
+axs.set_ylabel("Energy")
+axs.legend()
+plt.show()
 
 fig, axs = plt.subplots(1, 1, figsize=(8, 8))
 for i in range(4):
@@ -35,11 +46,6 @@ axs.set_ylabel("Entropy")
 axs.legend()
 plt.show()
 
-#
-# eig_vals, _ = np.linalg.eig(H)
-# perm = eig_vals.argsort()
-# print(eig_vals[perm])
-# eig_vals, _ = power_iteration(H, 400)
 # perm = eig_vals.argsort()
 # print(eig_vals[perm])
 #
