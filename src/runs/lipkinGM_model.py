e7420df4e0261807394420c548f26cbd43182482
Gregz9 <gregorkajda@gmail.com>
2023-05-19 02:11:34 +0200

    J=1 without W finished
    Signed-off-by: Gregz9 <gregorkajda@gmail.com>

--- src/runs/lipkinGM_model.py
+++ src/runs/lipkinGM_model.py
@@ -2,25 +2,38 @@ from __future__ import annotations
 from src.solvers import *
 from src.ops import *
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 # The case of spin J=1, solving for eigenvalues using analytical and numerical methods
 
-Vs = np.linspace(0.0, 2.0, 200)
-eigvals_an = np.zeros((len(Vs), 3))
-eigvals_num_PI = np.zeros((len(Vs), 3))
-eigvals_num_QR = np.zeros((len(Vs), 3))
 
-# for i, v in enumerate(tqdm(Vs)):
-#     H = lipkin_H_J1(v)
-#     eigvals, eigvecs = np.linalg.eig(H)
-#     eig_num_PI, _ = power_iteration(H, 1000)
-#     eig_num_QR = QR_solver(H, iters=1000)
-#     perm = eigvals.argsort()
-#     perm_num_PI = eig_num_PI.argsort()
-#     perm_num_QR = eig_num_QR.argsort()
-#     eigvals_an[i] = eigvals[perm]
-#     eigvals_num_PI[i] = eig_num_PI[perm_num_PI]
-#     eigvals_num_QR[i] = eig_num_QR[perm_num_QR]
+def plot_energy(energy_an, energy_vqe, v_values_ana, v_values_vqe):
+    fig, axs = plt.subplots(1, 1, figsize=(12, 12))
+    for i in range(len(energy_an[0])):
+        axs.plot(v_values_ana, energy_an[:, i], label=f"$E_{i}$")
+    axs.set_xlabel(r"$V/\epsilon$")
+    axs.scatter(v_values_vqe, energy_vqe, label="VQE Energy Scipy", marker="o")
+    axs.set_ylabel(r"$E/\epsilon$")
+    axs.legend(loc="upper left")
+    plt.show()
+
+
+Vs_an = np.linspace(0.0, 2.0, 200)
+eigvals_an = np.zeros((len(Vs_an), 3))
+eigvals_num_PI = np.zeros((len(Vs_an), 3))
+eigvals_num_QR = np.zeros((len(Vs_an), 3))
+
+for i, v in enumerate(tqdm(Vs_an)):
+    H = lipkin_H_J1(v)
+    eigvals, eigvecs = np.linalg.eig(H)
+    eig_num_PI, _ = power_iteration(H, 1000)
+    eig_num_QR = QR_solver(H, iters=1000)
+    perm = eigvals.argsort()
+    perm_num_PI = eig_num_PI.argsort()
+    perm_num_QR = eig_num_QR.argsort()
+    eigvals_an[i] = eigvals[perm]
+    eigvals_num_PI[i] = eig_num_PI[perm_num_PI]
+    eigvals_num_QR[i] = eig_num_QR[perm_num_QR]
 
 """ 
 In order for us to be able to use the VQE, we'll have to rewrite 
@@ -34,16 +47,18 @@ the Hamiltonian matrix/our ansatz is presented in "Quantum Computing
 for Programmers" written by Robert Hundt, page 252.
 """
 # Solving the eigenvalue problem using the VQE method
-Vs = np.linspace(0.0, 2.0, 20)
-learning_rate = 0.2
+Vs_vqe = np.linspace(0.0, 2.0, 10)
+learning_rate = 0.1
 momentum = 0.2
-num_shots = 1000
+num_shots = 2500
 max_epochs = 2500
-epochs = np.zeros(len(Vs))
-# min_energy = np.zeros(len(Vs))
-# for i, v in enumerate(tqdm(Vs)):
-angles = np.random.uniform(0.0, np.pi, size=4)
-angles, epoch, energy, delta_energy = VQE_momentum(
-    learning_rate, momentum, max_epochs, num_shots, angles, 2.0, J=1
-)
-print(f" V: {2.0}, Energy: {energy}, Epoch: {epoch}, Delta_energy: {delta_energy}")
+epochs = np.zeros(len(Vs_vqe))
+min_energy = np.zeros(len(Vs_vqe))
+for i, v in enumerate(tqdm(Vs_vqe)):
+    angles = np.random.uniform(0.0, np.pi, size=4)
+    angles, epoch, min_energy[i], delta_energy = VQE_momentum(
+        learning_rate, momentum, max_epochs, num_shots, angles, v, J=1
+    )
+    # print(f" V: {v}, Energy: {energy}, Epoch: {epoch}, Delta_energy: {delta_energy}")
+
+plot_energy(eigvals_an, min_energy, Vs_an, Vs_vqe)
