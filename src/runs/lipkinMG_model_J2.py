55073af910cb1214db17bbb293ef2fc2781761e1
Gregz9 <gregorkajda@gmail.com>
2023-05-29 01:57:32 +0200

    Final problem solved, however need to test if normal gradient calculations can even come close to the solution given by the scipy or numpy libraries
    Signed-off-by: Gregz9 <gregorkajda@gmail.com>

--- src/runs/lipkinMG_model_J2.py
+++ src/runs/lipkinMG_model_J2.py
@@ -18,6 +18,8 @@ for i, v in enumerate(tqdm(v_values_an)):
     eig_perm = eig_vals.argsort()
     eigvals_an[i], eig_vecs = eig_vals[eig_perm], eig_vecs[:, eig_perm]
 
+print(eigvals_an[49][0])
+
 
 # fig, axs = plt.subplots(1, 1, figsize=(8, 8))
 # for i in range(len(eigvals_an[0])):
@@ -34,3 +36,37 @@ for i, v in enumerate(tqdm(v_values_an)):
 # Proceeding from here, we shall prepare an 4 qubit ansatz and use it with our VQE
 # algorithm to solve for the lowest eigenvalue of the the lipkin system with a spin og
 # J=2, indicating four fermions.
+
+
+def get_gradient(angles, v, number_shots, unitaries):
+    grad = np.zeros(len(angles))
+    for index, angle in enumerate(angles):
+        tmp = angles.copy()
+        tmp[index] += np.pi / 2
+        energy_plus = measure_energy_mul(tmp, v, number_shots)
+        tmp[index] -= np.pi
+        energy_minus = measure_energy_mul(tmp, v, number_shots)
+        grad[index] = (energy_plus - energy_minus) / 2
+    return grad
+
+
+unitaries = prep_circuit_lipkin_J2()
+from scipy.optimize import minimize
+
+number_shots = 1000
+v_vals = np.array([1.0])  # only 2.0
+# target = eigvecs[:, 0] # ground state
+min_energy_scipy = np.zeros(len(v_vals))
+for index, v in enumerate(tqdm(v_vals)):
+    angles_start = np.random.uniform(low=0, high=2 * np.pi, size=5 * 8)
+    res = minimize(
+        fun=measure_energy_mul,
+        jac=get_gradient,
+        x0=angles_start,
+        args=(v, number_shots, unitaries),
+        method="BFGS",
+        options={"maxiter": 10000},
+        tol=1e-10,
+    )
+    min_energy_scipy[index] = res.fun
+    print(res)
