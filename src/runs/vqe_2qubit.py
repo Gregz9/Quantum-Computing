dc8fbd70e7b3e5446d8ab86486256bbec7e65d70
Gregz9 <gregorkajda@gmail.com>
2023-05-31 03:02:33 +0200

    Figures for 2 qubit system
    Signed-off-by: Gregz9 <gregorkajda@gmail.com>

--- src/runs/vqe_2qubit.py
+++ src/runs/vqe_2qubit.py
@@ -6,31 +6,34 @@ from src.ops import *
 from scipy.optimize import minimize
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+import time
 
 H = hamiltonian_2qubit(1.0, 2.0, 3.0, np.array([0.0, 2.5, 6.5, 7.0]))
 lmbds = np.linspace(0.0, 1.0, 50)
 eigvals_an = np.zeros((len(lmbds), H.shape[1]))
-eigvals_num1 = np.zeros((len(lmbds), H.shape[1]))
-eigvals_num2 = np.zeros((len(lmbds), H.shape[1]))
 entropy = np.zeros((len(lmbds), H.shape[1]))
 
+start_time = time.time()
 for i, lmbd in enumerate(lmbds):
     H = hamiltonian_2qubit(lmbd, 2.0, 3.0, np.array([0.0, 2.5, 6.5, 7.0]))
     eigvals, eigvecs = np.linalg.eig(H)
-    eig_num1, _ = power_iteration(H, 400)
-    eig_num2 = QR_solver(H)
     perm_an = eigvals.argsort()
-    perm_num1 = eig_num1.argsort()
-    perm_num2 = eig_num2.argsort()
     eigvals_an[i] = eigvals[perm_an]
-    eig_num1 = eig_num1[perm_num1]
-    eig_num2 = eig_num2[perm_num2]
-    eigvals_num1[i] = eig_num1
-    eigvals_num2[i] = eig_num2
     eigvecs = eigvecs[:, perm_an]
     entropy[i] = von_neumann(eigvals, eigvecs, 0)
+print(f"Time taken for the analytical method: {time.time() - start_time}")
 
+start_time = time.time()
+eigvals_num1 = np.zeros((len(lmbds), H.shape[1]))
+for i, lmbd in enumerate(lmbds):
+    H = hamiltonian_2qubit(lmbd, 2.0, 3.0, np.array([0.0, 2.5, 6.5, 7.0]))
+    eig_num1, _ = power_iteration(H, 400)
+    perm_num1 = eig_num1.argsort()
+    eig_num1 = eig_num1[perm_num1]
+    eigvals_num1[i] = eig_num1
+print(f"Time taken for the numerical method: {time.time() - start_time}")
 
+start_time = time.time()
 learning_rate = 0.2
 momentum = 0.2
 num_shots = 500
@@ -46,6 +49,7 @@ for i, lmbd in enumerate(tqdm(lmbds2)):
 # print(
 #     f"Lambda: {lmbds[i]}, Energy: {energy}, Epochs: {epoch}, Delta_energy : {delta_energy}"
 # )
+print(f"Time taken for the VQE method: {time.time() - start_time}")
 
 fig, axs = plt.subplots(1, 1, figsize=(8, 8))
 for i in range(4):
@@ -53,6 +57,21 @@ for i in range(4):
 axs.scatter(lmbds2, min_energy, label=f"$E_0")
 axs.set_xlabel(r"$\lambda$")
 axs.set_ylabel("Energy")
+axs.set_title(
+    "Energy levels of the two-body system as a function of interatction strength $\lambda$"
+)
+axs.legend()
+plt.show()
+
+fig, axs = plt.subplots(1, 1, figsize=(8, 8))
+for i in range(4):
+    axs.plot(lmbds, eigvals_num1[:, i], label=f"$E_{i}$")
+axs.scatter(lmbds2, min_energy, label=f"$E_0")
+axs.set_xlabel(r"$\lambda$")
+axs.set_ylabel("Energy")
+axs.set_title(
+    "Energy levels of the two-body system as a function of interatction strength $\lambda$"
+)
 axs.legend()
 plt.show()
 
@@ -61,5 +80,8 @@ for i in range(1):
     axs.plot(lmbds, entropy[:, i], label=f"$H_{i}$")
 axs.set_xlabel(r"$\lambda$")
 axs.set_ylabel("Entropy")
+axs.set_title(
+    "Von Neumann Entropy of a the two-body system as a function of interaction strength $\lambda$"
+)
 axs.legend()
 plt.show()
